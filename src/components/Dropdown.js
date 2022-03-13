ad3520ce25303185fc7ab646886e61cb7a0ad796
Joshua Jones <joshua.ar.jones@gmail.com>
2022-03-13 19:36:29 +0000

    implement list component from public source

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,11 +1,44 @@
 import "./Dropdown.css";
-import pokemon_data from "../data/pokemon_data";
+import pokemon_data from "../data/pokemon_data.json";
+import React, { useState, useEffect } from "react";
+
 function Dropdown() {
+  const demo = {
+    Pokemon: [
+      {
+        id: 1,
+        name: "bulbasaur",
+        egg_groups: [
+          { name: "monster", url: "https://pokeapi.co/api/v2/egg-group/1/" },
+          { name: "plant", url: "https://pokeapi.co/api/v2/egg-group/7/" },
+        ],
+      },
+    ],
+  };
+
+  const [data, setData] = useState(demo);
+  console.log(data);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("pokemon_data.json");
+      const newData = await response.json();
+      setData(newData);
+    };
+
+    fetchData();
+  }, []);
+
+  console.log(data);
+
   return (
     <div className="Dropdown">
       <p>Dropdown</p>
+
+      <hr />
+
       <ul>
-        {pokemon_data.Pokemon.map((item, key) => (
+        {data.Pokemon.map((item, key) => (
           <li key={key}>
             {item.id} {item.name}
             {item.egg_groups.map((item, key) => (
@@ -19,5 +52,4 @@ function Dropdown() {
     </div>
   );
 }
-console.log(pokemon_data);
 export default Dropdown;

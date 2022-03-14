8fe13c42629df13cb328609b02c1d14a92050053
Joshua Jones <joshua.ar.jones@gmail.com>
2022-03-14 01:02:47 +0000

    implement look up correctly for pkmn -> egg group

--- src/components/DropIn.js
+++ src/components/DropIn.js
@@ -3,12 +3,11 @@ import Box from "@mui/material/Box";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
+import InputLabel from "@mui/material/InputLabel";
 import { useState, useEffect } from "react";
 import "./Dropin.css";
 export default function DropIn() {
-  const [egg_groups, setAge] = React.useState([]); // set default pokemon to id 1
-  const [egg_mons, setEggMons] = React.useState([]);
-  const demo = {
+  const demo_pokemon = {
     Pokemon: [
       {
         id: 1,
@@ -19,9 +18,26 @@ export default function DropIn() {
         ],
       },
     ],
-  };
+  }; // dummy data
 
-  const [data, setData] = useState(demo);
+  const egg_list = {
+    id: 1,
+    name: "monster",
+
+    pokemon_species: [
+      {
+        name: "bulbasaur",
+        url: "https://pokeapi.co/api/v2/pokemon-species/1/",
+      },
+    ],
+  }; // dummy data
+
+  const [egg_groups, setEggGroups] = useState(
+    demo_pokemon.Pokemon[0].egg_groups
+  ); // egg groups come from the input dropdown pokemon selected
+  const [data, setData] = useState(demo_pokemon); //seed initial pokemon data with demo data
+  const [egg_mons, setEggMons] = useState(egg_list); // seed egg pokemon group array with demo data
+  const [currentSelection, setCurrentSelection] = useState("bulbasaur");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -31,49 +47,72 @@ export default function DropIn() {
     };
 
     fetchData();
-  }, []);
+    console.log("load list of pokemon");
+  }, []); // load drop down data of pokemon
 
   const handleChange = (event) => {
-    setAge(event.target.value);
+    setCurrentSelection(event.target.value);
   };
-  console.log(egg_groups);
-  var res = "no pokemon picked yet";
-  if (egg_groups.length === 0) {
-    //  block of code to be executed if the condition is true
-  } else {
-    //fetch the list of pokemon from the correct egg group
-    // test with monster, dragon, plant
-    const fetchData = async () => {
-      const response = await fetch("pokemon_data.json");
+  console.log(currentSelection);
+
+  useEffect(() => {
+    setEggGroups(
+      data.Pokemon.find((element) => element.name === currentSelection)
+        .egg_groups
+    ); //update value of egg groups when selected
+    console.log(egg_groups);
+  }, [currentSelection]);
+
+  // every time egg groups change pull in the pokemon from that egg group
+  // dummy for now looks up monster every time
+  useEffect(() => {
+    const fetch_mons = async () => {
+      const response = await fetch("monster.json");
       const newData = await response.json();
-      setData(newData);
+      setEggMons(newData);
     };
-    fetchData();
-    res = egg_groups.map((item, key) => <li key={key}>{item.name}</li>); //  block of code to be executed if the condition is false
-  }
+
+    fetch_mons();
+    console.log("fetched new egg mons");
+  }, [egg_groups]);
 
   return (
     <Box sx={{ minWidth: 120 }}>
+      <div className="pickNote">
+        <p>select a pokemon!</p>
+      </div>
       <FormControl fullWidth>
-        <div className="pickNote">
-          <p>select a pokemon!</p>
-        </div>
+        <InputLabel id="demo-simple-select-label">Pokemon</InputLabel>
         <Select
+          labelId="demo-simple-select-label"
           id="demo-simple-select"
-          value={egg_groups}
-          label="egg_groups"
+          value={currentSelection}
+          label="egggroups"
           onChange={handleChange}
         >
           {data.Pokemon.map((item, key) => (
-            <MenuItem key={key} value={item.egg_groups}>
+            <MenuItem key={key} value={item.name}>
               {item.name}
             </MenuItem>
           ))}
         </Select>
       </FormControl>
 
-      <h4>you picked : </h4>
-      <ul>{res}</ul>
+      <h4>you picked : {currentSelection}</h4>
+
+      <h5>egg groups:</h5>
+      <ul>
+        {egg_groups.map((item, key) => (
+          <li>{item.name}</li>
+        ))}
+      </ul>
+
+      <h4>compatible pokemon:</h4>
+      <ul>
+        {egg_mons.pokemon_species.map((item, key) => (
+          <li key={key}>{item.name}</li>
+        ))}
+      </ul>
     </Box>
   );
 }
